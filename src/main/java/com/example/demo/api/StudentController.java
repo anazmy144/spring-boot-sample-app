7d06bf1a664f41643f5dce34a26c3106bfa62c5b
anazmy14 <ahmed.nazmy@sia-world.com>
2022-01-10 00:17:38 +0200

    student init

---
+++ src/main/java/com/example/demo/api/StudentController.java
@@ -0,0 +1,15 @@
+package com.example.demo.api;
+
+import com.example.demo.model.Student;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/student")
+public class StudentController {
+    @GetMapping
+    public Student getStudent(){
+        return new Student("ahmed",  24);
+    }
+}
